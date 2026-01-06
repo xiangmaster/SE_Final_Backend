6c15218eb816c5b694096880c14173a83f6887d8
YiheHuang <yih2943997328@outlook.com>
2026-01-06 13:45:07 +0800

    初始化

---
+++ src/main/java/com/javaee/se_final_backend/SeFinalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.javaee.se_final_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeFinalBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeFinalBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/javaee/se_final_backend/SeFinalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaee.se_final_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeFinalBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
